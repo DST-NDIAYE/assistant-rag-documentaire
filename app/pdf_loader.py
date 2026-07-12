377dadf6cf6a146daf0a993fe12bdbbf62187c45
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2026-07-12 03:47:19 +0200

    nouveau dossier notebook

--- app/pdf_loader.py
+++ app/pdf_loader.py
@@ -1,1 +1,15 @@
-documents
+import fitz
+
+
+def extraire_texte_pdf(chemin_pdf):
+    doc = fitz.open(chemin_pdf)
+    print(f"le document contient {len(doc)} pages ")
+
+    liste_page = [ ]
+    for page in doc:
+        texte = page.get_text()
+        if texte.strip() != "" :
+            liste_page.append(texte)
+        texte_complet = "\n".join(liste_page)
+    return texte_complet
+
